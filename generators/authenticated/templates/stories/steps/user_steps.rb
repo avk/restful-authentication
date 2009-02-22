16c7a34b91465a7ab9a320d786a3d6663b0590b5
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-02-22 01:40:25 -0800

    commented out extraneous require '/../helper' in user_steps.rb

--- generators/authenticated/templates/stories/steps/user_steps.rb
+++ generators/authenticated/templates/stories/steps/user_steps.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../helper'
+# require File.dirname(__FILE__) + '/../helper'
 
 RE_<%= file_name.capitalize %>      = %r{(?:(?:the )? *(\w+) *)}
 RE_<%= file_name.capitalize %>_TYPE = %r{(?: *(\w+)? *)}
